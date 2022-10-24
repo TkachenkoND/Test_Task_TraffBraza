3e610705dc1201b0cd85b972473b7887c05dccdf
TkachenkoND00 <tkachenkond00@gmail.com>
2022-10-24 22:58:10 +0300

    initial commit

---
+++ app/src/main/java/com/testproject/test_task_traffbraza/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.testproject.test_task_traffbraza
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
